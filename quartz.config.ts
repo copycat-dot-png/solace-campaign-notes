de0bb76f5e420ee9ce14f076d644bd5a609f16ce
Aoife <acecookie@live.com>
2024-05-22 18:12:35 +0100

    Quartz sync: May 22, 2024, 6:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Solace Setting Information",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
